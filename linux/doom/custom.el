8ba22b5956dd2643022178cf9357fb228ae40647
Ole Magnus Johnsen <hei@omfj.no>
2022-07-02 19:00:22 +0200

    updated structure

---
+++ linux/doom/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f7fed1aadf1967523c120c4c82ea48442a51ac65074ba544a5aefc5af490893b" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
