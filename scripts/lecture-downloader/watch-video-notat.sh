817bec89f7fc124f3a68207d0f970f80bd93c984
Ole Magnus Fon Johnsen <me@omfj.no>
2022-07-21 03:59:59 +0200

    cleaning

--- scripts/lecture-downloader/watch-video-notat.sh
+++
@@ -1,3 +0,0 @@
-stream_video=$(youtube-dl -f best --get-url https://pstreaming01.app.uib.no/vnlive/498cd9ae-d3dc-441a-9dd1-5adb55590aa1-presentation-delivery.stream/media_w237889768_576.ts)
-
-ffmpeg -i $stream_video out.mp4
