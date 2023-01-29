5b39c580ef656cfdc9ef460fa93c097aca9b24de
Ole Magnus Fon Johnsen <me@omfj.no>
2023-01-30 00:45:07 +0100

    new nvim config

---
+++ hosts/common/config/nvim/lua/plugins/init.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "j-hui/fidget.nvim",
+      "folke/neodev.nvim",
+    },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      pcall(require("nvim-treesitter.install").update { with_sync = true })
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+  },
+
+  -- Git
+  "tpope/vim-fugitive",
+  "tpope/vim-rhubarb",
+  "lewis6991/gitsigns.nvim",
+
+  "lukas-reineke/indent-blankline.nvim", -- Add indentation guides even on blank lines
+  "numToStr/Comment.nvim", -- "gc" to comment visual regions/lines
+  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
+  { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable "make" == 1 },
+}
+
