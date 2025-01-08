a970e0ba1b85409857bcec10c6086882ee740676
Ole Magnus Fon Johnsen <me@omfj.no>
2025-01-08 20:17:43 +0100

    Use bright colors for comments

--- hosts/common/config/nvim/lua/plugins/treesitter.lua
+++ hosts/common/config/nvim/lua/plugins/treesitter.lua
@@ -20,6 +20,14 @@ return {
         "svelte",
         "yaml",
       },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
     },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      vim.api.nvim_set_hl(0, "@comment", { fg = "#FFA500" })
+    end,
   },
 }
