ea6af773ecfcf26d1b09984d967421570016344a
Ole Magnus Fon Johnsen <me@omfj.no>
2025-01-08 18:09:14 +0100

    Add new neovim config

---
+++ hosts/common/config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    config = true,
+    opts = {
+      size = 20,
+      direction = "float",
+      float_opts = {
+        border = "curved",
+        winblend = 3,
+      },
+    },
+  },
+}
