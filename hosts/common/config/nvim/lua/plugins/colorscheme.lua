bcdcabcfdc31cbc5a527307198ed784e244d3c03
Ole Magnus Fon Johnsen <me@omfj.no>
2025-08-30 21:03:46 +0200

    VSCode colorscheme

--- hosts/common/config/nvim/lua/plugins/colorscheme.lua
+++ hosts/common/config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
-	{
-		"aktersnurra/no-clown-fiesta.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = true,
-		priority = 1000,
-	},
+    {
+        "aktersnurra/no-clown-fiesta.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        "Mofiqul/vscode.nvim",
+        lazy = true,
+        priority = 1000,
+        config = function()
+            require("vscode").setup({
+                italic_comments = true,
+            })
+        end,
+    },
 }
