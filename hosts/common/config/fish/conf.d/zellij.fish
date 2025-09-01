8cac6dda1f325df9994b021e84edbda7c831feb2
Ole Magnus Fon Johnsen <me@omfj.no>
2025-09-01 15:34:32 +0200

    feat(zellij): enhance terminal multiplexer configuration and workflow

--- hosts/common/config/fish/conf.d/zellij.fish
+++ hosts/common/config/fish/conf.d/zellij.fish
@@ -2,5 +2,5 @@ if not set -q ZELLIJ
     if not zellij list-sessions 2>/dev/null | grep -q "base"
         zellij -s base -d
     end
-    zellij attach base
+    zellij --layout ~/.config/zellij/layouts/default.kdl attach base
 end
---
+++ hosts/common/config/zellij/README.md
@@ -0,0 +1,7 @@
+Install plugins
+
+```bash
+mkdir -p ~/.config/zellij/plugins
+wget -O ~/.config/zellij/plugins/zjstatus.wasm https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm
+```
+
