b1473624441848b32cd2b0808b8084511247f5fb
Ole Magnus Fon Johnsen <me@omfj.no>
2025-09-01 17:25:30 +0200

    refactor(fish): switch from vi to default key bindings

--- hosts/common/config/fish/conf.d/zellij.fish
+++ hosts/common/config/fish/conf.d/zellij.fish
@@ -2,5 +2,5 @@ if not set -q ZELLIJ
     if not zellij list-sessions 2>/dev/null | grep -q "base"
         zellij -s base -d
     end
-    zellij --layout ~/.config/zellij/layouts/default.kdl attach base
+    zellij attach base
 end
