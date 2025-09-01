2ed87cb2fdbfdab21a5a0c9eeec2859ec39785d2
Ole Magnus Fon Johnsen <me@omfj.no>
2025-09-01 14:33:46 +0200

    feat(shell): configure default zellij session and disable tmux autostart

--- hosts/common/config/fish/conf.d/tmux.fish
+++ hosts/common/config/fish/conf.d/tmux.fish
@@ -1,5 +1,5 @@
-if not set -q TMUX
-    set -g TMUX tmux new-session -d -s base
-    eval $TMUX
-    tmux attach-session -d -t base
-end
+# if not set -q TMUX
+#     set -g TMUX tmux new-session -d -s base
+#     eval $TMUX
+#     tmux attach-session -d -t base
+# end
