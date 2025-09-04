9af617b7a1d04105e765bea56f6cf960f826959b
Ole Magnus Fon Johnsen <me@omfj.no>
2025-09-04 20:46:25 +0200

    chore(config): add zed configuration and update tmux and fish settings

--- hosts/common/config/fish/conf.d/zellij.fish
+++ hosts/common/config/fish/conf.d/zellij.fish
@@ -1,6 +1,6 @@
-if not set -q ZELLIJ
-    if not zellij list-sessions 2>/dev/null | grep -q "base"
-        zellij -s base -d
-    end
-    zellij attach base
-end
+# if not set -q ZELLIJ
+#     if not zellij list-sessions 2>/dev/null | grep -q "base"
+#         zellij -s base -d
+#     end
+#     zellij attach base
+# end
