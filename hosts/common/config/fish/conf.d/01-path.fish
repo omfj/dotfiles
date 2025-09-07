29f32c68056846e8b6086bb549bdf83deb86df2f
Ole Magnus Fon Johnsen <me@omfj.no>
2025-09-07 17:07:40 +0200

    feat(fish): update shell configuration paths and add icu4c support

--- hosts/common/config/fish/conf.d/01-path.fish
+++ hosts/common/config/fish/conf.d/01-path.fish
@@ -1,17 +1,19 @@
-fish_add_path $HOME/.ghcup/bin
-fish_add_path $HOME/.cargo/bin
-fish_add_path $HOME/.emacs.d/bin
-fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.deno/bin
-fish_add_path $HOME/.atuin/bin/
-fish_add_path $HOME/.composer/vendor/bin
-fish_add_path $HOME/go/bin
+fish_add_path $HOME/.ghcup/bin # GHCup for Haskell 
+fish_add_path $HOME/.cargo/bin # Rust
+fish_add_path $HOME/.emacs.d/bin # Emacs
+fish_add_path $HOME/.local/bin # User local binaries
+fish_add_path $HOME/.deno/bin # Deno
+fish_add_path $HOME/.atuin/bin/ # Atuin for shell history
+fish_add_path $HOME/.composer/vendor/bin # PHP Composer
+fish_add_path $HOME/go/bin # Go binaries
+fish_add_path $HOME/.slack/bin # Slack CLI
 
-fish_add_path $BUN_INSTALL/bin
+fish_add_path $BUN_INSTALL/bin # Bun JavaScript runtime
 
 fish_add_path /usr/local/bin
 fish_add_path /usr/local/sbin
-fish_add_path /usr/local/opt/openjdk/bin
+
+fish_add_path /usr/local/opt/openjdk/bin # OpenJDK
 
 # Homebrew
 fish_add_path /opt/homebrew/bin
@@ -22,8 +24,8 @@ fish_add_path /opt/homebrew/opt/openjdk@17/bin
 fish_add_path /opt/homebrew/opt/sqlite/bin
 fish_add_path /opt/homebrew/opt/rustup/bin
 
-fish_add_path /Library/Developer/CommandLineTools/usr/bin
-fish_add_path /Library/Frameworks/Python.framework/Versions/3.10/bin
+fish_add_path /Library/Developer/CommandLineTools/usr/bin # Xcode Command Line Tools
+fish_add_path /Library/Frameworks/Python.framework/Versions/3.10/bin # Python 3.10
 
 # Add PNPM to PATH if not already there
 if not contains $PNPM_HOME $PATH
