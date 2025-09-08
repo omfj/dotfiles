5064dfb0b742be1140f8e87116ccfd05697d7548
Ole Magnus Fon Johnsen <me@omfj.no>
2025-09-09 00:05:46 +0200

    chore(neovim): update plugin configuration and remove unused plugins

---
+++ hosts/common/config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		notifier = { enabled = true },
+		words = { enabled = true },
+	},
+}
