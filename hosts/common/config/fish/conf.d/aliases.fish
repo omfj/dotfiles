cb90f0ae0851fd2a4490c117a93003ca80a0bf92
Ole Magnus Fon Johnsen <me@omfj.no>
2025-11-22 18:53:32 +0100

    update configs

--- hosts/common/config/fish/conf.d/aliases.fish
+++ hosts/common/config/fish/conf.d/aliases.fish
@@ -6,8 +6,6 @@ abbr -a ll "eza -l"
 abbr -a lla "eza -la"
 abbr -a i "pnpm i"
 abbr -a cat "bat"
-abbr -a uib "cd $HOME/Google\ Drive/My\ Drive/uib/"
-abbr -a minecraft "cd $HOME/Library/Application\ Support/minecraft"
 
 # Common git commands
 abbr -a g "git"
@@ -34,8 +32,4 @@ abbr -a .......... "cd ../../../../../../.."
 # Project navigation
 abbr -a dots "cd $DOTFILES"
 abbr -a conf "cd $HOME/.config"
-
-# Docker aliases
-abbr -a dcu "docker-compose up --build"
-abbr -a dcd "docker-compose down"
 
