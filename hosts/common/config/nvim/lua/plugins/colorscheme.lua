2caf64470e509ef7dea9ebc5b1a6eaf9dde58880
Ole Magnus Fon Johnsen <me@omfj.no>
2026-02-09 02:21:57 +0100

    chore: update Ghostty, Neovim plugins and colorscheme

--- hosts/common/config/nvim/lua/plugins/colorscheme.lua
+++ hosts/common/config/nvim/lua/plugins/colorscheme.lua
@@ -43,6 +43,21 @@ return {
 		"wtfox/jellybeans.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = {}, -- Optional
+		opts = {
+			transparent = true,
+		},
+	},
+	{
+		"roerohan/orng.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("orng").setup({
+				variant = "dark", -- "dark" or "light"
+				transparent = false,
+				italic_comment = true,
+			})
+			-- vim.cmd("colorscheme orng")
+		end,
 	},
 }
