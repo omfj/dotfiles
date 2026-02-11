ca91825a73fd22d57f8d6f37883cd1ebe77c22b3
Ole Magnus Fon Johnsen <me@omfj.no>
2026-02-11 08:51:15 +0100

    fish stuff

--- hosts/obuntu/config/fish/conf.d/fnm.fish
+++ hosts/obuntu/config/fish/conf.d/fnm.fish
@@ -1,3 +1,17 @@
 if command -q fnm
     fnm env --use-on-cd --shell fish | source
 end
+
+# fnm
+set FNM_PATH "/home/ojohnsen/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
+
+# fnm
+set FNM_PATH "/home/ojohnsen/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
