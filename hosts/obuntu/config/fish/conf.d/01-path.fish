bbabbd6a03561b8e3d82bf6a39fcb50ee5c89d1e
Ole Magnus Fon Johnsen <me@omfj.no>
2026-03-11 14:16:33 +0100

    feat: update harpoon keymap and go path

--- hosts/obuntu/config/fish/conf.d/01-path.fish
+++ hosts/obuntu/config/fish/conf.d/01-path.fish
@@ -3,11 +3,13 @@ fish_add_path $HOME/.cargo/bin # Rust
 fish_add_path $HOME/.local/bin # User local binaries
 fish_add_path $HOME/.deno/bin # Deno
 fish_add_path $HOME/.atuin/bin/ # Atuin for shell history
+fish_add_path $HOME/.atuin/bin/ # Atuin for shell history
 fish_add_path $HOME/go/bin # Go binaries
 
 fish_add_path $BUN_INSTALL/bin # Bun JavaScript runtime
 
 fish_add_path /usr/local/bin
+fish_add_path /usr/local/go/bin/ # Go
 
 # Add PNPM to PATH if not already there
 if not contains $PNPM_HOME $PATH
