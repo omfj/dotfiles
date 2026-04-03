fe6c84ff1f3bbefca335c72c6466b4b8b356fd9d
Ole Magnus Fon Johnsen <me@omfj.no>
2026-04-03 12:58:03 +0200

    feat(nvim): add LSP utilities and improve code formatting

--- hosts/common/config/nvim/plugin/treesitter.lua
+++ hosts/common/config/nvim/plugin/treesitter.lua
@@ -3,7 +3,9 @@ vim.api.nvim_create_autocmd("PackChanged", {
 		local name, kind = ev.data.spec.name, ev.data.kind
 		if name == "nvim-treesitter" and kind == "update" then
 			vim.schedule(function()
-				if not ev.data.active then vim.cmd.packadd("nvim-treesitter") end
+				if not ev.data.active then
+					vim.cmd.packadd("nvim-treesitter")
+				end
 				vim.cmd("TSUpdate")
 			end)
 		end
@@ -23,7 +25,6 @@ local ensure_installed = {
 	"javascript",
 	"jsdoc",
 	"json",
-	"jsonc",
 	"lua",
 	"luadoc",
 	"luap",
