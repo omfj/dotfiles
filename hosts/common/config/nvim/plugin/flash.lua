7f3612d68a97b3e5abfdd04fbdbf99fe81e0325b
Ole Magnus Fon Johnsen <me@omfj.no>
2026-04-05 02:38:14 +0200

    refactor(nvim): consolidate plugins and switch to mini.* alternatives

--- hosts/common/config/nvim/plugin/flash.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add({
-	{ src = "https://github.com/folke/flash.nvim" },
-})
-
-require("flash").setup()
-
-vim.keymap.set({ "n", "x", "o" }, "s", function()
-	require("flash").jump()
-end, { desc = "Flash" })
--- hosts/common/config/nvim/plugin/mini.lua
+++ hosts/common/config/nvim/plugin/mini.lua
@@ -18,9 +18,34 @@ require("mini.pairs").setup()
 require("mini.comment").setup()
 require("mini.move").setup()
 require("mini.ai").setup()
-require("mini.hipatterns").setup({
+local hipatterns = require("mini.hipatterns")
+hipatterns.setup({
 	highlighters = {
-		hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
+		hex_color = hipatterns.gen_highlighter.hex_color(),
+		fixme = { pattern = "%f[%w]()FIXME:?()%f[%W]", group = "DiagnosticError" },
+		hack = { pattern = "%f[%w]()HACK:?()%f[%W]", group = "DiagnosticWarn" },
+		todo = { pattern = "%f[%w]()TODO:?()%f[%W]", group = "DiagnosticOk" },
+		note = { pattern = "%f[%w]()NOTE:?()%f[%W]", group = "DiagnosticInfo" },
+		warn = { pattern = "%f[%w]()WARN:?()%f[%W]", group = "DiagnosticWarn" },
 	},
 })
 require("mini.bufremove").setup()
+
+local sessions = require("mini.sessions")
+sessions.setup({ autowrite = true })
+vim.keymap.set("n", "<leader>qs", function()
+	sessions.read()
+end, { desc = "Restore session" })
+vim.keymap.set("n", "<leader>qS", function()
+	sessions.select()
+end, { desc = "Select session" })
+vim.keymap.set("n", "<leader>ql", function()
+	sessions.read()
+end, { desc = "Restore last session" })
+vim.keymap.set("n", "<leader>qd", function()
+	require("mini.sessions").config.autowrite = false
+end, { desc = "Don't save session" })
+
+require("mini.jump2d").setup({
+	mappings = { start_jumping = "s" },
+})
