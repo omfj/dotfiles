5e97fa155583c6c1326f59315b0245ea6fc43f08
Ole Magnus Fon Johnsen <me@omfj.no>
2026-04-16 19:50:00 +0200

    feat(nvim): add jinja template language support

--- hosts/common/config/nvim/plugin/conform.lua
+++ hosts/common/config/nvim/plugin/conform.lua
@@ -32,6 +32,7 @@ require("conform").setup({
 		toml = { "taplo" },
 		rust = { "rustfmt" },
 		zig = { "zigfmt" },
+		jinja = { "djlint" },
 	},
 	format_on_save = function(bufnr)
 		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
