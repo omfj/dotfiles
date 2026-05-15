687500a7c5b5623ecc01c5dda23896d699712bdf
Ole Magnus Fon Johnsen <me@omfj.no>
2026-05-15 21:50:26 +0200

    feat(nvim): add oil.nvim file explorer plugin configuration

---
+++ hosts/common/config/nvim/plugin/oil.lua
@@ -0,0 +1,18 @@
+vim.pack.add({
+	{ src = "https://github.com/stevearc/oil.nvim" },
+})
+
+require("oil").setup({
+	default_file_explorer = false,
+	view_options = {
+		show_hidden = true,
+	},
+	float = {
+		padding = 2,
+		max_width = 80,
+		max_height = 30,
+		border = "single",
+	},
+})
+
+vim.keymap.set("n", "<leader>to", "<cmd>Oil --float<cr>", { desc = "Open oil (float)" })
