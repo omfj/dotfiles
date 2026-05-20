0fc5ae7a551c4cc41d3ee17df14a57b0e5a9994f
Ole Magnus Fon Johnsen <me@omfj.no>
2026-05-20 11:52:33 +0200

    feat(nvim): code fold and bibtex

--- hosts/common/config/nvim/plugin/ufo.lua
+++ hosts/common/config/nvim/plugin/ufo.lua
@@ -1,31 +1,31 @@
-vim.pack.add({
-	{ src = "https://github.com/kevinhwang91/nvim-ufo" },
-	{ src = "https://github.com/kevinhwang91/promise-async" },
-})
+local marker_open = { "//#startregion", "//#region", "{{{" }
+local marker_close = { "//#endregion", "}}}" }
 
-require("ufo").setup({
-	provider_selector = function()
-		return { "treesitter", "indent" }
-	end,
-})
+local function matches_any(line, patterns)
+	for _, p in ipairs(patterns) do
+		if line:find(p, 1, true) then
+			return true
+		end
+	end
+	return false
+end
 
-vim.keymap.set("n", "zR", function()
-	require("ufo").openAllFolds()
-end, { desc = "Open all folds" })
-vim.keymap.set("n", "zM", function()
-	require("ufo").closeAllFolds()
-end, { desc = "Close all folds" })
-vim.keymap.set("n", "zr", function()
-	require("ufo").openFoldsExceptKinds()
-end, { desc = "Open folds except kinds" })
-vim.keymap.set("n", "zm", function()
-	require("ufo").closeFoldsWith()
-end, { desc = "Close folds with" })
+function _G.custom_foldexpr()
+	local line = vim.api.nvim_buf_get_lines(0, vim.v.lnum - 1, vim.v.lnum, false)[1] or ""
+	if matches_any(line, marker_open) then
+		return ">1"
+	elseif matches_any(line, marker_close) then
+		return "<1"
+	end
+	return vim.lsp.foldexpr()
+end
+
+vim.opt.foldexpr = "v:lua.custom_foldexpr()"
+
 vim.keymap.set("n", "K", function()
-	local foldclosed = vim.fn.foldclosed(vim.fn.line("."))
-	if foldclosed ~= -1 then
-		require("ufo").peekFoldedLinesUnderCursor()
+	if vim.fn.foldclosed(vim.fn.line(".")) ~= -1 then
+		vim.cmd("normal! zo")
 	else
 		vim.lsp.buf.hover()
 	end
-end, { desc = "Peek fold or hover" })
+end, { desc = "Open fold or hover" })
