748abc9cf56b8cbfd7cdb4833a47811d390766b4
Ole Magnus Fon Johnsen <me@omfj.no>
2026-05-22 13:45:26 +0200

    prefix helpers

--- dotty.lua
+++ dotty.lua
@@ -2,46 +2,66 @@ local hostname = dotty.hostname()
 
 local link = dotty.link
 
+local function common(path)
+	return "hosts/common/" .. path
+end
+
+local function obook(path)
+	return "hosts/obook/" .. path
+end
+
+local function obuntu(path)
+	return "hosts/obuntu/" .. path
+end
+
+local function oarch(path)
+	return "hosts/oarch/" .. path
+end
+
+local function config(path)
+	return "~/.config/" .. path
+end
+
 -- Common
-link("hosts/common/config/ghostty", "~/.config/ghostty")
-link("hosts/common/config/helix", "~/.config/helix")
-link("hosts/common/config/starship.toml", "~/.config/starship.toml")
-link("hosts/common/vimrc", "~/.vimrc")
-link("hosts/common/config/atuin", "~/.config/atuin")
-link("hosts/common/config/lazydocker", "~/.config/lazydocker")
-link("hosts/common/config/lazygit", "~/.config/lazygit")
-link("hosts/common/config/cheat", "~/.config/cheat")
-link("hosts/common/config/tmux", "~/.config/tmux")
-link("hosts/common/config/nvim", "~/.config/nvim")
+link(common("config/ghostty"), config("ghostty"))
+link(common("config/helix"), config("helix"))
+link(common("config/starship.toml"), config("starship.toml"))
+link(common("vimrc"), "~/.vimrc")
+link(common("config/atuin"), config("atuin"))
+link(common("config/lazydocker"), config("lazydocker"))
+link(common("config/lazygit"), config("lazygit"))
+link(common("config/cheat"), config("cheat"))
+link(common("config/tmux"), config("tmux"))
+link(common("config/nvim"), config("nvim"))
 
 -- Shared between main computers
 if hostname == "obook.local" or hostname == "oarch" then
-	link("hosts/common/config/git", "~/.config/git")
+	link(common("config/git"), config("git"))
 end
 
 -- Mac (obook)
 if hostname == "obook.local" then
-	link("hosts/obook/config/fish", "~/.config/fish")
-	link("hosts/common/config/zed", "~/.config/zed")
-	link("hosts/obook/zshrc", "~/.zshrc")
+	link(obook("config/fish"), config("fish"))
+	link(common("config/zed"), config("zed"))
+	link(obook("zshrc"), "~/.zshrc")
 end
 
 -- Arch (oarch)
 if hostname == "oarch" then
-	link("hosts/oarch/config/fish", "~/.config/fish")
-	link("hosts/oarch/config/bspwm", "~/.config/bspwm")
-	link("hosts/oarch/config/sxhkd", "~/.config/sxhkd")
-	link("hosts/oarch/config/dunst", "~/.config/dunst")
-	link("hosts/oarch/config/picom", "~/.config/picom")
-	link("hosts/oarch/config/rofi", "~/.config/rofi")
-	link("hosts/oarch/config/polybar", "~/.config/polybar")
-	link("hosts/oarch/config/betterlockscreenrc", "~/.config/betterlockscreenrc")
-	link("hosts/oarch/config/user-dirs.dirs", "~/.config/user-dirs.dirs")
-	link("hosts/oarch/config/user-dirs.locale", "~/.config/user-dirs.locale")
-	link("hosts/oarch/config/hypr", "~/.config/hypr")
+	link(oarch("config/fish"), config("fish"))
+	link(oarch("config/bspwm"), config("bspwm"))
+	link(oarch("config/sxhkd"), config("sxhkd"))
+	link(oarch("config/dunst"), config("dunst"))
+	link(oarch("config/picom"), config("picom"))
+	link(oarch("config/rofi"), config("rofi"))
+	link(oarch("config/polybar"), config("polybar"))
+	link(oarch("config/betterlockscreenrc"), config("betterlockscreenrc"))
+	link(oarch("config/user-dirs.dirs"), config("user-dirs.dirs"))
+	link(oarch("config/user-dirs.locale"), config("user-dirs.locale"))
+	link(oarch("config/hypr"), config("hypr"))
 end
 
 -- obuntu
 if dotty.file_exists("~/.obuntu") then
-	link("hosts/obuntu/zshrc", "~/.zshrc")
+	link(obuntu("zshrc"), "~/.zshrc")
 end
